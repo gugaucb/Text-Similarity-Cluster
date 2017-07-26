ca7044625cb87e2543f9db2465c516f0103b875f
tr300869 <tr300869@dipsi05-trf1.trf1.gov.br>
2017-07-26 15:52:16 -0300

    Include Apache Tika and Spark test in main class.

--- text-similarity-cluster/src/main/java/me/costa/gustavo/app/App.java
+++ text-similarity-cluster/src/main/java/me/costa/gustavo/app/App.java
@@ -1,13 +1,30 @@
 package me.costa.gustavo.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import static spark.Spark.get;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Logger;
+
+import org.apache.tika.Tika;
+import org.apache.tika.exception.TikaException;
+import org.xml.sax.SAXException;
+
+public class App {
+	private final static Logger LOGGER = Logger.getLogger(App.class.getName());
+	
+	public static void main(String[] args) throws IOException, SAXException, TikaException {
+		LOGGER.info("Hello World!");
+		get("/hello", (req, res) -> "Hello World");
+		LOGGER.info(parseToStringExample());
+		
+	}
+	
+	public static String parseToStringExample() throws IOException, SAXException, TikaException {
+	    Tika tika = new Tika();
+	    try (InputStream stream = new FileInputStream(App.class.getClassLoader().getResource("teste.pdf").getPath())) {
+	        return tika.parseToString(stream);
+	    }
+	}
 }
--- text-similarity-cluster/src/test/java/me/costa/gustavo/app/AppTest.java
+++ text-similarity-cluster/src/test/java/me/costa/gustavo/app/AppTest.java
@@ -1,14 +1,10 @@
 package me.costa.gustavo.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+    
 {
     /**
      * Create the test case
@@ -17,22 +13,22 @@ public class AppTest
      */
     public AppTest( String testName )
     {
-        super( testName );
+       // super( testName );
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
+   /* public static Test suite()
     {
-        return new TestSuite( AppTest.class );
-    }
+       // return new TestSuite( AppTest.class );
+    }*/
 
     /**
      * Rigourous Test :-)
      */
     public void testApp()
     {
-        assertTrue( true );
+        //assertTrue( true );
     }
 }
