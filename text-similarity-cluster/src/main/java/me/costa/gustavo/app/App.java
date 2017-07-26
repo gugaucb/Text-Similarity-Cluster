fe254c34222e09282927062901341f5887d5e7e6
tr300869 <tr300869@dipsi05-trf1.trf1.gov.br>
2017-07-26 17:10:39 -0300

    Include autoDetectParse in Main class and PDF file for example

--- text-similarity-cluster/src/main/java/me/costa/gustavo/app/App.java
+++ text-similarity-cluster/src/main/java/me/costa/gustavo/app/App.java
@@ -1,14 +1,14 @@
 package me.costa.gustavo.app;
 
-import static spark.Spark.get;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Logger;
 
-import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.SAXException;
 
 public class App {
@@ -16,15 +16,20 @@ public class App {
 	
 	public static void main(String[] args) throws IOException, SAXException, TikaException {
 		LOGGER.info("Hello World!");
-		get("/hello", (req, res) -> "Hello World");
-		LOGGER.info(parseToStringExample());
+		LOGGER.info("------------------------------------------------------------------");
+		LOGGER.info(autoDetectParseToStringExample().trim());
 		
 	}
 	
-	public static String parseToStringExample() throws IOException, SAXException, TikaException {
-	    Tika tika = new Tika();
+	public static String autoDetectParseToStringExample() throws IOException, SAXException, TikaException {
+		BodyContentHandler handler = new BodyContentHandler();
+	    AutoDetectParser parser = new AutoDetectParser();
+	    Metadata metadata = new Metadata();
 	    try (InputStream stream = new FileInputStream(App.class.getClassLoader().getResource("teste.pdf").getPath())) {
-	        return tika.parseToString(stream);
+	        parser.parse(stream, handler, metadata);
+	        stream.close();
+	        return handler.toString();
 	    }
+	    
 	}
 }
